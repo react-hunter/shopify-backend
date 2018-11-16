3480a73aea8776f2b07f1f1c0cd30adaf901da79
leonachan1 <1nomadproductions@gmail.com>
2018-11-16 08:11:01 -0700

    success in testing of webhook

--- controllers/feeds/webhook.js
+++ controllers/feeds/webhook.js
@@ -11,8 +11,54 @@ var csrfMiddleware = lusca.csrf();
  * POST /
  * Feed Trigger Action
  */
+
+var productHookList = {};
 exports.index = (req, res) => {
     
-    console.log('request body: ', req);
-    res.json({status: 'ok'});
+    var hookHeaders = req.headers;
+    var hookBody = req.body;
+
+    var temp = hookHeaders['x-shopify-shop-domain'].split('.');
+    var fromStore = temp[0];
+    
+    // productHookList[fromStore + '-update'] = [];
+    if (hookHeaders['x-shopify-topic'] == 'products/update') {
+        if (contains.call(productHookList[fromStore + '-update'], hookHeaders['x-shopify-product-id'])) {
+            console.log('request headers: ', hookHeaders);
+            console.log('request body: ', hookBody);
+            console.log('////////////////////////////////////////////////////////////////////');
+            res.json({status: 'ok'});
+            productHookList[fromStore + '-update'] = [];
+        } else {
+            productHookList[fromStore + '-update'] = [];
+            productHookList[fromStore + '-update'].push(hookHeaders['x-shopify-product-id']);
+        }
+    }
 };
+
+var contains = function(needle) {
+    // Per spec, the way to identify NaN is that it is not equal to itself
+    var findNaN = needle !== needle;
+    var indexOf;
+
+    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
+        indexOf = Array.prototype.indexOf;
+    } else {
+        indexOf = function(needle) {
+            var i = -1, index = -1;
+
+            for(i = 0; i < this.length; i++) {
+                var item = this[i];
+
+                if((findNaN && item !== item) || item === needle) {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        };
+    }
+
+    return indexOf.call(this, needle) > -1;
+}
