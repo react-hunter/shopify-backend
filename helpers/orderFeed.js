bbb8a34336da903ee573373508631543d161b5eb
leonachan1 <1nomadproductions@gmail.com>
2019-01-21 16:37:34 -0700

    prevent unfulfilled items

--- helpers/orderFeed.js
+++ helpers/orderFeed.js
@@ -190,150 +190,152 @@ module.exports = {
         await delay(2000)
 
         order.line_items.forEach((item, index) => {
-            var orderData = {}
-            // orderData.order_number = order.order_number
-            orderData.order_number = orderRow.outgoingOrderNumbers[index]
-            orderData.order_date = order.created_at.substr(5, 2) + '/' + order.created_at.substr(8, 2) + '/' + order.created_at.substr(0, 4)
-            orderData.order_payment_method = orderRow.orderPaymentMethod
-            orderData.transaction_id = orderRow.transactionId
-            if (order.shipping_lines.length > 0) {
-                orderData.ship_method = order.shipping_lines[0].source
-            } else {
-                orderData.ship_method = ''
-            }
-            orderData.subtotal = order.subtotal_price
-            orderData.tax_total = order.total_tax
-            orderData.discount_total = order.total_discounts
-            // orderData.total_total = parseFloat(order.subtotal_price) + parseFloat(order.total_tax) - parseFloat(order.total_discounts)
-            orderData.total_total = order.total_price
+            if (item.fulfillment_status != null) {
+                var orderData = {}
+                // orderData.order_number = order.order_number
+                orderData.order_number = orderRow.outgoingOrderNumbers[index]
+                orderData.order_date = order.created_at.substr(5, 2) + '/' + order.created_at.substr(8, 2) + '/' + order.created_at.substr(0, 4)
+                orderData.order_payment_method = orderRow.orderPaymentMethod
+                orderData.transaction_id = orderRow.transactionId
+                if (order.shipping_lines.length > 0) {
+                    orderData.ship_method = order.shipping_lines[0].source
+                } else {
+                    orderData.ship_method = ''
+                }
+                orderData.subtotal = order.subtotal_price
+                orderData.tax_total = order.total_tax
+                orderData.discount_total = order.total_discounts
+                // orderData.total_total = parseFloat(order.subtotal_price) + parseFloat(order.total_tax) - parseFloat(order.total_discounts)
+                orderData.total_total = order.total_price
 
-            orderData.customer_email = ''
-            if (order.customer) {
-                orderData.customer_email = order.customer.email
-            }
+                orderData.customer_email = ''
+                if (order.customer) {
+                    orderData.customer_email = order.customer.email
+                }
 
-            if (order.shipping_address) {
-                orderData.ship_firstname = order.shipping_address.first_name
-                orderData.ship_lastname = order.shipping_address.last_name
-                orderData.ship_street = order.shipping_address.address1
-                orderData.ship_street2 = order.shipping_address.address2
-                orderData.ship_postal_code = order.shipping_address.zip
-                orderData.ship_city = order.shipping_address.city
-                orderData.ship_state = orderRow.shipState
-                orderData.ship_country = order.shipping_address.country
-                orderData.ship_phone = order.shipping_address.phone
-            }
+                if (order.shipping_address) {
+                    orderData.ship_firstname = order.shipping_address.first_name
+                    orderData.ship_lastname = order.shipping_address.last_name
+                    orderData.ship_street = order.shipping_address.address1
+                    orderData.ship_street2 = order.shipping_address.address2
+                    orderData.ship_postal_code = order.shipping_address.zip
+                    orderData.ship_city = order.shipping_address.city
+                    orderData.ship_state = orderRow.shipState
+                    orderData.ship_country = order.shipping_address.country
+                    orderData.ship_phone = order.shipping_address.phone
+                }
 
-            if (order.billing_address) {
-                orderData.bill_firstname = order.billing_address.first_name
-                orderData.bill_lastname = order.billing_address.last_name
-                orderData.bill_street = order.billing_address.address1
-                orderData.bill_street2 = order.billing_address.address2
-                orderData.bill_postal_code = order.billing_address.zip
-                orderData.bill_city = order.billing_address.city
-                orderData.bill_state = orderRow.billState
-                orderData.bill_country = order.billing_address.country
-                orderData.bill_phone = order.billing_address.phone
-            }
+                if (order.billing_address) {
+                    orderData.bill_firstname = order.billing_address.first_name
+                    orderData.bill_lastname = order.billing_address.last_name
+                    orderData.bill_street = order.billing_address.address1
+                    orderData.bill_street2 = order.billing_address.address2
+                    orderData.bill_postal_code = order.billing_address.zip
+                    orderData.bill_city = order.billing_address.city
+                    orderData.bill_state = orderRow.billState
+                    orderData.bill_country = order.billing_address.country
+                    orderData.bill_phone = order.billing_address.phone
+                }
 
-            if (item.fulfillment_status == 'fulfilled') {
-                orderData.item_status = 'shipped'
-            } else if (item.fulfillment_status == 'partial' || item.fulfillment_status == null) {
-                orderData.item_status = 'ordered'
-            } else {
-                orderData.item_status = 'cancelled'
-            }
-            // Get variant by using item.variant_id
-            // Initialize item_orig_price at first because it should match the order of header.
-            orderData.item_sku = ''
-            orderData.item_orig_price = 0
-            shopify.productVariant.get(item.variant_id).then(
-                variant => {
-                    orderData.item_sku = variant.id
-                    orderData.item_orig_price = variant.compare_at_price
-                },
-                variantError => console.log('Error in getting product variant: ', variantError)
-            )
-            orderData.item_price = item.price
-            orderData.item_qty_ordered = item.quantity
-            orderData.item_qty_shipped = item.quantity - item.fulfillable_quantity
-            if (item.fulfillment_status == null) {
-                orderData.item_qty_cancelled = 0
-            } else {
-                orderData.item_qty_cancelled = item.fulfillable_quantity
-            }
-            var taxes = 0.0
-            if (item.tax_lines.length > 0) {
-                item.tax_lines.forEach((tax) => {
-                    taxes += parseFloat(tax.price)
-                })
-            }
-            orderData.item_tax = taxes
-            var discounts = 0.0
-            if (item.discount_allocations.length > 0) {
-                item.discount_allocations.forEach((dis) => {
-                    discounts += parseFloat(dis.amount)
-                })
-            }
-            orderData.item_discount = discounts
-            orderData.item_total = (parseFloat(item.price) + taxes - discounts) * item.quantity
+                if (item.fulfillment_status == 'fulfilled') {
+                    orderData.item_status = 'shipped'
+                } else if (item.fulfillment_status == 'partial') {
+                    orderData.item_status = 'ordered'
+                } else {
+                    orderData.item_status = 'cancelled'
+                }
+                // Get variant by using item.variant_id
+                // Initialize item_orig_price at first because it should match the order of header.
+                orderData.item_sku = ''
+                orderData.item_orig_price = 0
+                shopify.productVariant.get(item.variant_id).then(
+                    variant => {
+                        orderData.item_sku = variant.id
+                        orderData.item_orig_price = variant.compare_at_price
+                    },
+                    variantError => console.log('Error in getting product variant: ', variantError)
+                )
+                orderData.item_price = item.price
+                orderData.item_qty_ordered = item.quantity
+                orderData.item_qty_shipped = item.quantity - item.fulfillable_quantity
+                if (item.fulfillment_status == null) {
+                    orderData.item_qty_cancelled = 0
+                } else {
+                    orderData.item_qty_cancelled = item.fulfillable_quantity
+                }
+                var taxes = 0.0
+                if (item.tax_lines.length > 0) {
+                    item.tax_lines.forEach((tax) => {
+                        taxes += parseFloat(tax.price)
+                    })
+                }
+                orderData.item_tax = taxes
+                var discounts = 0.0
+                if (item.discount_allocations.length > 0) {
+                    item.discount_allocations.forEach((dis) => {
+                        discounts += parseFloat(dis.amount)
+                    })
+                }
+                orderData.item_discount = discounts
+                orderData.item_total = (parseFloat(item.price) + taxes - discounts) * item.quantity
 
-            orderData.final_sale = false
-            orderData.order_gift_sender = ''
-            orderData.order_gift_recepient = ''
-            orderData.order_gift_message = ''
+                orderData.final_sale = false
+                orderData.order_gift_sender = ''
+                orderData.order_gift_recepient = ''
+                orderData.order_gift_message = ''
 
-            orderData.auth_code = order.checkout_token
-            orderData.tracking_number = ''
-            if (order.shipping_lines.length > 0) {
-                orderData.ship_carrier = order.shipping_lines[0].source
-            }
-            orderData.invoice_amount = order.total_price
-            orderData.retailer_order_number = order.order_number + ' | ' + order.id + ' | ' + item.id
+                orderData.auth_code = order.checkout_token
+                orderData.tracking_number = ''
+                if (order.shipping_lines.length > 0) {
+                    orderData.ship_carrier = order.shipping_lines[0].source
+                }
+                orderData.invoice_amount = order.total_price
+                orderData.retailer_order_number = order.order_number + ' | ' + order.id + ' | ' + item.id
 
-            var fulfillmentId = 0
-            if (order.fulfillments.length > 0) {
-                order.fulfillments.forEach((fulfillment) => {
-                    if (fulfillment.status == 'success') {
-                        fulfillmentId = parseInt(fulfillment.id)
-                        let fulfillmentCreateDate = new Date(fulfillment.created_at)
-                        orderData.ship_date = (fulfillmentCreateDate.getMonth() + 1) + '/' + fulfillmentCreateDate.getDate() + '/' + fulfillmentCreateDate.getFullYear()
-                        if (fulfillment.line_items.length > 0) {
-                            try {
-                                fulfillment.line_items.forEach(fulfillmentItem => {
-                                    if (fulfillmentItem.id == item.id) {
-                                        orderData.tracking_number = fulfillment.tracking_number
-                                        throw BreakException
-                                    }
-                                })
-                            } catch (e) {
-                                if (e !== BreakException) throw e
+                var fulfillmentId = 0
+                if (order.fulfillments.length > 0) {
+                    order.fulfillments.forEach((fulfillment) => {
+                        if (fulfillment.status == 'success') {
+                            fulfillmentId = parseInt(fulfillment.id)
+                            let fulfillmentCreateDate = new Date(fulfillment.created_at)
+                            orderData.ship_date = (fulfillmentCreateDate.getMonth() + 1) + '/' + fulfillmentCreateDate.getDate() + '/' + fulfillmentCreateDate.getFullYear()
+                            if (fulfillment.line_items.length > 0) {
+                                try {
+                                    fulfillment.line_items.forEach(fulfillmentItem => {
+                                        if (fulfillmentItem.id == item.id) {
+                                            orderData.tracking_number = fulfillment.tracking_number
+                                            throw BreakException
+                                        }
+                                    })
+                                } catch (e) {
+                                    if (e !== BreakException) throw e
+                                }
                             }
                         }
-                    }
-                })
-            } else {
-                orderData.ship_date = 'No Ship yet'
+                    })
+                } else {
+                    orderData.ship_date = 'No Ship yet'
+                }
+                if (fulfillmentId > 0) {
+                    shopify.fulfillmentEvent.list(order.id, fulfillmentId)
+                    .then((events) => {
+                        if (events.length > 0) {
+                            events.forEach((event) => {
+                                if (event.status == 'in_transit')
+                                    orderData.delivery_date = event.estimated_delivery_at
+                                else
+                                    orderData.delivery_date = event.delivery_date
+                            })
+                        } else {
+                            orderData.delivery_date = 'Not Sure'
+                        }
+                    })
+                    .catch(err => console.log('fulfillmentEvent error: ', err))
+                } else {
+                    orderData.delivery_date = 'Not Sure'
+                }
+                orderDataList.push(orderData)
             }
-            if (fulfillmentId > 0) {
-                shopify.fulfillmentEvent.list(order.id, fulfillmentId)
-                .then((events) => {
-                    if (events.length > 0) {
-                        events.forEach((event) => {
-                            if (event.status == 'in_transit')
-                                orderData.delivery_date = event.estimated_delivery_at
-                            else
-                                orderData.delivery_date = event.delivery_date
-                        })
-                    } else {
-                        orderData.delivery_date = 'Not Sure'
-                    }
-                })
-                .catch(err => console.log('fulfillmentEvent error: ', err))
-            } else {
-                orderData.delivery_date = 'Not Sure'
-            }
-            orderDataList.push(orderData)
         })
 
         await delay(3000)
